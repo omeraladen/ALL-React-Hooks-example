dbd4300d3428155cd05ac4a6eb8a5a67c23cb5ad
omeraladen <killerpop1738@gmail.com>
2022-08-28 10:44:28 +0200

    Done with useState() with 3 examples ✔

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React  from "react";
+import UseState from "./Hooks/UseState";
+
+
+
+const App = () => {
+  
+  
+  return (
+    <div>
+      <UseState/>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/Hooks/UseState.jsx
@@ -0,0 +1,76 @@
+
+import React, { useState } from 'react';
+
+//  example (1) simple increment number
+
+const UseState = () => {
+  
+  let [ counter , setCounter ] = useState(0)
+  const increment = () => {
+    setCounter(counter + 1)
+  }
+  return (
+    <div>
+      {counter}
+      <button onClick={increment}>increment</button>
+    </div>
+  )
+}
+
+
+/* example (2) typing something and displayed to screen same time
+
+const UseState = () => {
+  let [inputValue , setInputValue] = useState('🚗');
+
+  let changeState = (event) => {
+    let newValue = event.target.value;
+    console.log(newValue)
+    setInputValue(newValue) 
+
+  }
+
+  return (
+    <div>
+      <input onChange={changeState} placeholder='type Car...'  />
+      {inputValue}
+    </div>
+  )
+}
+*/
+
+
+/* use Class Component with No Hook
+class UseState extends React.Component{
+  constructor(){
+    super();
+
+    this.state ={
+      count:0
+    };
+  }
+  
+  Increment = () => {
+    this.setState({
+      count : this.state.count + 1
+    })
+ }
+  render(){
+    
+
+    
+  
+    return (
+      <div>
+       <p>the count = {this.state.count}</p>
+       <button onClick={this.Increment}>click </button>
+    </div>
+    )
+  }
+}
+*/
+
+
+
+export default UseState;
+
